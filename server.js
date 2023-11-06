ac03bd035296d3fb02490dc705e948da332f5a00
MichelC345 <michel.caesarr@gmail.com>
2023-11-05 23:35:28 -0300

    Ajustes 05/11

--- server.js
+++ server.js
@@ -56,11 +56,11 @@ app.get("/usuario", checkNotAuthenticated, (req, res) => { //tela do usuário em
 app.post("/cadastro", async (req, res) => {
     let {user, pass, pass2, phone, date} = req.body;
     //regExp = expressão regular para validar telefone
-    //fonte: https://pt.stackoverflow.com/questions/282311/valida%C3%A7%C3%A3o-para-campo-tipo-telefone
-    var regExp = /^[0-9]{2}?(?:\()[0-9]{2}(?:\))?[0-9]{4,5}[0-9]{4}$/;
-    //var regExp = /^(?:\+)[0-9]{2}\s?(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/;
+    //construída com a ferramenta Simple Regex Language
+    //fonte: https://pt.stackoverflow.com/questions/189187/express%C3%A3o-regular-em-javascript-para-telefone-com-ddi/189192
+    var regExp = /[0-9]{2}[0-9]{2}[0-9]{4,5}[0-9]{4}$/;
 
-    console.log({
+    console.log({ //dados inseridos são mostrados no console
         user,
         pass,
         pass2,
@@ -73,13 +73,16 @@ app.post("/cadastro", async (req, res) => {
     if (!user || !pass || !pass2 || !phone || !date) {
         errors.push({message: "Por favor preencha todos os campos."});
     }
+    if (user.length < 4) {
+        errors.push({message: "Usuário deve ter no mínimo 4 caracteres"});
+    }
     if (pass.length < 6) {
         errors.push({message: "A senha deve ter no mínimo 6 caracteres."});
     }
     if (pass != pass2) {
         errors.push({message: "As senhas não procedem."});
     }
-    if (!regExp.test(date)) {
+    if (!regExp.test(phone)) {
         errors.push({message: "Telefone inserido é inválido."});
         errors.push({message: "Formato aceito: código do páis/DDD/telefone"})
         errors.push({message: "Exemplo de telefone válido: 5545233215454"});
@@ -92,7 +95,7 @@ app.post("/cadastro", async (req, res) => {
         let hashedPass = await bcrypt.hash(pass, 10); //criptografia
         console.log(hashedPass);
 
-        pool.query( //requisição ao banco de dados
+        pool.query( //requisição ao banco de dados verificando se já há usuário com o nome
             `SELECT * FROM public.dadoscrud2
             WHERE usuario = $1`,
             [user],
@@ -100,26 +103,44 @@ app.post("/cadastro", async (req, res) => {
                 if (err) {
                     throw err;
                 }
-                console.log("reaches here");
+                console.log("Usuário com mesmo nome encontrado aqui");
                 console.log(results.rows);
 
                 if (results.rows.length > 0) { //usuário já cadastrado?
                     errors.push({ message: "Este usuário já está registrado." });
                     res.render("cadastro", { errors }); //permanece na tela e relata o erro
                 }else {
-                    pool.query (
-                        `INSERT INTO public.dadoscrud2 (usuario, senha, telefone, data_nasc)
-                        VALUES ($1, $2, $3, $4)
-                        RETURNING id, senha`, //returning - forma de atribuir ID
-                        [user, hashedPass, phone, date], 
-                        (err, results) => {
-                            if (err) {
-                                throw err;
-                            }
-                            console.log(results.rows);
-                            req.flash("success_msg", "Cadastro feito com sucesso.");
-                            res.redirect("/");
+                    pool.query ( //requisiçao ao banco de dados para verificar se já há um telefone igual
+                    `SELECT * FROM public.dadoscrud2
+                    WHERE telefone = $1`,
+                    [phone], //parâmetro $1 = telefone inserido
+                    (err, results) => {
+                        if (err) {
+                            throw err;
                         }
+                        console.log("Usuário com mesmo telefone encontrado aqui");
+                        console.log(results.rows);
+
+                        if (results.rows.length > 0) { //telefone já cadastrado?
+                            errors.push({ message: "O telefone inserido já está registrado." });
+                            res.render("cadastro", { errors }); //permanece na tela e relata o erro
+                        }else { //cadastro pode ser efetuado
+                            pool.query ( //requisição ao banco de dados para inserção dos dados
+                                `INSERT INTO public.dadoscrud2 (usuario, senha, telefone, data_nasc)
+                                VALUES ($1, $2, $3, $4)
+                                RETURNING id, senha`, //returning - forma de atribuir ID
+                                [user, hashedPass, phone, date], //parâmetros $1, $2, $3, $4. Portanto, senha inserida será a criptografada
+                                (err, results) => {
+                                    if (err) {
+                                        throw err;
+                                    }
+                                    console.log(results.rows);
+                                    req.flash("success_msg", "Cadastro feito com sucesso.");
+                                    res.redirect("/");
+                                }
+                            );
+                        }
+                    }
                     );
                 }
             }
